3b3087a4f19b6341f7d5458dca839b15ad677cc3
ogedi001 <ogedifavour2@gmail.com>
2026-02-05 15:31:00 +0100

    feat: auth  modules

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { FastifyRequest } from 'fastify';
+
+import { TokenService } from '../services/token.service';
+import { UnauthorizedException } from 'src/common/exceptions';
+import { ErrorCodes } from 'src/common/constants/error-codes';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly tokenService: TokenService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET || 'fx_secret_key',
+      passReqToCallback: true, //  important
+    });
+  }
+
+  async validate(req: FastifyRequest, payload: any) {
+    console.log('✅ JwtStrategy validate called');
+    console.log('payload:', payload);
+
+    const token = ExtractJwt.fromAuthHeaderAsBearerToken()(req);
+
+    console.log('token:', token);
+
+    if (!token) {
+      throw new UnauthorizedException(
+        ErrorCodes.AUTH_TOKEN_MISSING,
+        'Missing token',
+      );
+    }
+
+    const blacklisted = await this.tokenService.isTokenBlacklisted(token);
+    if (blacklisted) {
+      throw new UnauthorizedException(
+        ErrorCodes.AUTH_UNAUTHORIZED,
+        'Token has been revoked',
+      );
+    }
+
+    return {
+      id: payload.sub,
+      email: payload.email,
+      role: payload.role,
+    };
+  }
+}
